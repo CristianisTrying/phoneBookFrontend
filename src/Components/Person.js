23c78837dc05fdec9f197ef1fa1953eeb2cda51f
CristianIsTrying <cjimenez6960@gmail.com>
2023-03-27 16:39:42 -0700

    Adding frontend phoneBook application

---
+++ src/Components/Person.js
@@ -0,0 +1,9 @@
+const Persons = ({person, deletePerson}) => {
+    return ( 
+        <li>
+            {person.name} - {person.number} <button onClick={() => deletePerson(person.id)}>Delete</button>
+        </li>
+     );
+}
+ 
+export default Persons;
